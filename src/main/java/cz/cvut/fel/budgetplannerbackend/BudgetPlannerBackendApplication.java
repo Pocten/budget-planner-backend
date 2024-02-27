2a375ca3201477f5cc8e04c5402069d4d8ccf084
Pocten <neohawk51@gmail.com>
2024-02-27 13:59:12 +0100

    repository initialization

---
+++ src/main/java/cz/cvut/fel/budgetplannerbackend/BudgetPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.budgetplannerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetPlannerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetPlannerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/cvut/fel/budgetplannerbackend/BudgetPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.budgetplannerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPlannerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
