c832f7db94dd82d62cd71a79d71a9f0236ec4040
Pocten <neohawk51@gmail.com>
2024-02-27 14:34:31 +0100

    Heroku connection

--- src/main/java/cz/cvut/fel/budgetplannerbackend/BudgetPlannerBackendApplication.java
+++ src/main/java/cz/cvut/fel/budgetplannerbackend/BudgetPlannerBackendApplication.java
@@ -2,10 +2,17 @@ package cz.cvut.fel.budgetplannerbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BudgetPlannerBackendApplication {
 
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Budget Planner with CI/CD??????????";
+    }
     public static void main(String[] args) {
         SpringApplication.run(BudgetPlannerBackendApplication.class, args);
     }
