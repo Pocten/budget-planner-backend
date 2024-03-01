a3ddc8c90257abdefcb0100a745dd9892f15207b
Pocten <neohawk51@gmail.com>
2024-03-01 16:50:12 +0100

    Fixed 'UserMapper'

--- src/main/java/cz/cvut/fel/budgetplannerbackend/mapper/UserMapper.java
+++ src/main/java/cz/cvut/fel/budgetplannerbackend/mapper/UserMapper.java
@@ -2,11 +2,20 @@ package cz.cvut.fel.budgetplannerbackend.mapper;
 
 import cz.cvut.fel.budgetplannerbackend.dto.UserDto;
 import cz.cvut.fel.budgetplannerbackend.entity.User;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    UserDto toDto(User user);
-    User toEntity(UserDto dto);
 
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "userName", target = "userName")
+    @Mapping(source = "userEmail", target = "userEmail")
+    @Mapping(source = "userPassword", target = "userPassword")
+    @Mapping(source = "userDateRegistration", target = "userDateRegistration")
+    UserDto toDto(User user);
+
+    @InheritInverseConfiguration
+    User toEntity(UserDto dto);
 }
